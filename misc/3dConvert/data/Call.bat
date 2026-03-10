972e0eb2c3755275e4325c38e45c6ff15f6831ee
Amaury Chevoir <amaury.chevoir@gmail.com>
2026-03-10 10:54:51 +0100

    Latest game updates (now 100% blend1-free)

--- misc/3dConvert/data/Call.bat
+++ misc/3dConvert/data/Call.bat
@@ -1,1 +1,1 @@
-Noesis.exe ?cmode "C:\Users\amaury\Downloads\Stranger_Things_Bike_g3d\model.obj" "C:\Users\amaury\Downloads\Stranger_Things_Bike_g3d\model.dae" -nopause -flipuv -objmtl
+Noesis.exe ?cmode "C:\Users\amaury\Downloads\stranger_things_bike_big_g3d\model.obj" "C:\Users\amaury\Downloads\stranger_things_bike_big_g3d\model.fbx" -nopause -flipuv -objmtl
